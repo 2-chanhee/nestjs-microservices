4205ac0f1ed492caf3165567eb32b2c1d5ccddcd
2-chanhee <l01083582454@gmail.com>
2022-07-16 22:46:06 +0900

    backend create user api & event (TCP)

--- nestjs-microservices-backend/src/app.controller.ts
+++ nestjs-microservices-backend/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
 import { AppService } from './app.service';
+import { CreateUserRequest } from './create_user_request.tdo';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
-    @Get()
-    getHello(): string {
-        return this.appService.getHello();
+    @Post()
+    createUser(@Body() createUserRequest: CreateUserRequest) {
+        this.appService.createUser(createUserRequest);
     }
 }
